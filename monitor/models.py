6c894c11c3bd34edfe21ea189779fc4039e4bbc5
Arimatea Neto <arineto30@gmail.com>
2017-05-24 15:28:05 -0300

    Using tweepy to reply a tweet

--- monitor/models.py
+++ monitor/models.py
@@ -6,6 +6,8 @@ from twitter_monitor.twitter_api import api
 
 class TwitterUser(models.Model):
 
+    user_id = models.CharField(max_length=20)
+
     username = models.CharField(max_length=100, unique=True)
 
     status = models.IntegerField(
@@ -27,7 +29,8 @@ class TwitterUser(models.Model):
             tweets = user.timeline(count=200)
             for tweet in tweets:
                 Tweet.objects.create(
-                    user=self, text=tweet.text, date=tweet.created_at
+                    tweet_id=tweet.id, user=self, text=tweet.text,
+                    date=tweet.created_at
                 )
             self.update_status(enums.TwitterUserStatusEnum.VALID)
         except TweepError:
@@ -35,6 +38,8 @@ class TwitterUser(models.Model):
 
 
 class Tweet(models.Model):
+
+    tweet_id = models.CharField(max_length=20)
 
     user = models.ForeignKey(TwitterUser)
 
@@ -49,8 +54,16 @@ class Tweet(models.Model):
             self.user, self.date.strftime('%d/%m/%Y %H:%I')
         )
 
+    def reply(self, user, text):
+        tweet = api.update_status(text, in_reply_to_status_id=self.tweet_id)
+        TweetResponse.objects.create(
+            response_id=tweet.id, user=user, tweet=self, text=text
+        )
+
 
 class TweetResponse(models.Model):
+
+    response_id = models.CharField(max_length=20)
 
     user = models.ForeignKey('users.User')
 
